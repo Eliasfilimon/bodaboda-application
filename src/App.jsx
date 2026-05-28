1c0f34c60702bcdfc33bc9eddb7fe4ce3026aef5
elly23 <143171436+Eliasfilimon@users.noreply.github.com>
2026-05-28 12:23:30 +0300

    feat: add ProtectedRoute to App.jsx, add PaymentPage, NotificationsPage, AdminDashboardPage routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { BottomNav } from "./components/BottomNav";
 import { OfflineBanner } from "./components/OfflineBanner";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 import { HomePage } from "./pages/HomePage";
 import { RequestRidePage } from "./pages/RequestRidePage";
 import { RiderDashboardPage } from "./pages/RiderDashboardPage";
@@ -14,6 +15,9 @@ import { RiderLoginPage } from "./pages/RiderLoginPage";
 import { TripHistoryPage } from "./pages/TripHistoryPage";
 import { RateTripPage } from "./pages/RateTripPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { PaymentPage } from "./pages/PaymentPage";
+import { NotificationsPage } from "./pages/NotificationsPage";
+import { AdminDashboardPage } from "./pages/AdminDashboardPage";
 
 function App() {
   return (
@@ -21,17 +25,64 @@ function App() {
       <OfflineBanner />
       <Navbar />
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/request" element={<RequestRidePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/rider-register" element={<RiderRegisterPage />} />
         <Route path="/rider-login" element={<RiderLoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/history" element={<TripHistoryPage />} />
-        <Route path="/rate/:tripId" element={<RateTripPage />} />
-        <Route path="/track" element={<TrackRidePage />} />
-        <Route path="/dashboard" element={<RiderDashboardPage />} />
+
+        {/* Protected - any authenticated user */}
+        <Route path="/request" element={
+          <ProtectedRoute requiredType="user">
+            <RequestRidePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/track" element={
+          <ProtectedRoute>
+            <TrackRidePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/history" element={
+          <ProtectedRoute>
+            <TripHistoryPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/rate/:tripId" element={
+          <ProtectedRoute>
+            <RateTripPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/payment" element={
+          <ProtectedRoute>
+            <PaymentPage />
+          </ProtectedRoute>
+        } />
+        <Route path="/notifications" element={
+          <ProtectedRoute>
+            <NotificationsPage />
+          </ProtectedRoute>
+        } />
+
+        {/* Protected - rider only */}
+        <Route path="/dashboard" element={
+          <ProtectedRoute requiredType="rider">
+            <RiderDashboardPage />
+          </ProtectedRoute>
+        } />
+
+        {/* Admin - open in dev, protect with role in production */}
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <AdminDashboardPage />
+          </ProtectedRoute>
+        } />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <BottomNav />
